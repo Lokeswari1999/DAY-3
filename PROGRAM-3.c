2fc6e7399ce4a67407c44d42f8d93e7d0c5838b9
Lokeswari1999 <108974119+Lokeswari1999@users.noreply.github.com>
2022-07-14 14:27:50 +0530

    Create PROGRAM-3.c

---
+++ PROGRAM-3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+int num,temp=0;
+printf("eneter the num ");
+scanf("%d",&num);
+if(num<=999)
+{
+while(num>0)
+{
+    temp=temp*10+num%10;
+    num=num/10;
+}
+}
+printf("reverse the num %d",temp);
+}
